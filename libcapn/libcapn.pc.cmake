da3902d0d668836594227b74d412977b4106dfb7
semen.darienko <semen.darienko@wildix.com>
2020-07-24 02:18:04 +0300

    Depricate libapn and use libcurl for push request to remote http service

--- libcapn/libcapn.pc.cmake
+++
@@ -1,6 +0,0 @@
-Name: lib${CAPN_LIB_NAME}
-Description: ${PROJECT_NAME}
-Version: ${CAPN_VERSION}
-Libs: -L${CAPN_INSTALL_PATH_LIB} -l${CAPN_LIB_NAME}
-Libs.private: -lopenssl
-Cflags: -I${CAPN_INSTALL_PATH_INCLUDES} ${CAPN_C_FLAGS}
